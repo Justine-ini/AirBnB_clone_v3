d85bb929cc2782d59adda97d2c1fff2203799b69
Justine-ini <106019828+Justine-ini@users.noreply.github.com>
2024-01-10 02:00:42 +0100

    Delete api directory

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Blueprint initialization for the views."""
-
-from flask import Blueprint
-
-
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
