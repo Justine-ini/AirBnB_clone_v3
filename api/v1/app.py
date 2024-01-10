86109db4b27c625316c85908de631be48abed355
Justine-ini <inijustine4040@gmail.com>
2024-01-09 17:13:51 -0800

    Updated methods to handle errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,52 @@
 #!/usr/bin/python3
-"""Update Amenity"""
+'''Update Amenity.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 
 app = Flask(__name__)
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Closes the session"""
+def tear_down_flask(exception):
+    '''Closes the session'''
+
+    # print(exception)
     storage.close()
 
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
